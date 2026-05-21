70665582cee918ca5f3b4721bf8d1cf09616b6e6
Mr penington <benjaminbkris@gmail.com>
2026-05-21 14:01:20 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT ||3000;
 
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.get('/', (req, res) => {
