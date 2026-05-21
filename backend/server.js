e6e0c1430905ed9aad06c6515852af01b044c968
Mr penington <benjaminbkris@gmail.com>
2026-05-21 14:40:21 +0200

    e

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,30 @@ const PORT = process.env.PORT ||3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
+app.post('/submit', (req, res) => {
+    pool.query(`
+        INSERT INTO table (text)
+        VALUES ($1)
+        `, [req.body.text], (err, result) => {
+        if (err) {
+            console.error('Error inserting data into database:', err);
+            res.status(500).json({ error: 'Internal server error' });
+        } else {
+            res.json({ message: 'Form submitted successfully!' });
+        }
+    });
+});
+
+
+/*SERVER START*/
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
