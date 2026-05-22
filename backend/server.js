418051f401b91b9be2fd4bc81e7151371a91404e
Mr penington <benjaminbkris@gmail.com>
2026-05-22 12:57:52 +0200

    e

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,9 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from "url";
 import pg from "pg";
-import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT ||3000;
-
-app.use(cors({
-  origin: "http://localhost:3000", // adjust if your frontend is elsewhere
-  credentials: true
-}));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
