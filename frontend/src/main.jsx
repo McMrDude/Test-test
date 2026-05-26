0fa6ddeb99ccc9024cd66705658cfcca495b6241
Mr penington <benjaminbkris@gmail.com>
2026-05-26 14:33:54 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Register from './components/Register.jsx'
-import Login from './components/login.jsx'
-import Forgot from './components/forgot-password.jsx'
-import Reset from './components/reset-password.jsx'
+import App from './app.jsx'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
