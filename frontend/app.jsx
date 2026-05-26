15e3f6d747a7fa31a6512bee5eab2c51cf762b61
Mr penington <benjaminbkris@gmail.com>
2026-05-26 14:01:38 +0200

    Update app.jsx

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -3,3 +3,18 @@ import { Link } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="card">
+      <h1>Vite + React</h1>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+    </div>
+  )
+}
+
+export default App
