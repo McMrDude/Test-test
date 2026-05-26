112716007a8d5cba200c0589aee83395d471497b
Mr penington <benjaminbkris@gmail.com>
2026-05-26 14:29:51 +0200

    Update app.jsx

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -8,12 +8,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="card">
-      <h1>Vite + React</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </div>
+    <>
+        <div className="card">
+        <h1>Vite + React</h1>
+        <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+        </button>
+        </div>
+    </>
   )
 }
 
